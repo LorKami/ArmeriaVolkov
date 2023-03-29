0363953b7a50020cfd295a86c732d0f7d93904db
LorKami <mauriciodsa12@gmail.com>
2023-03-29 03:59:37 -0600

    modified:   package.json 	modified:   public/card.png 	new file:   src/components/Footer/Footer.css 	new file:   src/components/Footer/index.js 	modified:   src/components/Monitor/Monitor.css 	new file:   src/images/double_5.png 	modified:   src/pages/Home.js

---
+++ src/components/Footer/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import '../Footer/Footer.css'
+
+const Footer = () => {
+  return (
+    <div>
+        <div className='Footer'>
+            <div className='FooterImage'>
+                2023 - Armeria Volkova | By: LorKami
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Monitor from "../components/Monitor"
 import Header from "../components/Header"
+import Footer from "../components/Footer"
 
 const Home = () => {
   return (
     <div>
         <Header />
         <Monitor />
+        <Footer />
     </div>
   )
 }
